63de886cfa0a45e04ca17ba79db2cf6df708f049
aaps <aaps@gmail.com>
2016-02-07 16:13:08 +0100

    fininsh getcolors2.py for now, this will put less load on the webserver of grahamedgecombe.com, only when things change on his side will it download !

--- getcolors.py
+++ getcolors.py
@@ -1,100 +1,102 @@
-#!/usr/bin/python -B
+# #!/usr/bin/python -B
 
-import urllib2
-from bs4 import BeautifulSoup
-import re
-from PIL import Image
-import StringIO
-import ast
-import json
-from staticvalues import *
-from material import Material
-import os.path
-import base64
+#use getcolors2.py !
+
+# import urllib2
+# from bs4 import BeautifulSoup
+# import re
+# from PIL import Image
+# import StringIO
+# import ast
+# import json
+# from staticvalues import *
+# from material import Material
+# import os.path
+# import base64
 
 
 
-f = open('materials.json', 'w')
+# f = open('materials.json', 'w')
 
-materials = {}
-textures = {}
-baseurl = 'http://minecraft-ids.grahamedgecombe.com/'
-cssurl = baseurl + 'stylesheets/bundles/all/1440429950.css'
-imageurl = baseurl + 'images/sprites/items-21.png'
+# materials = {}
+# textures = {}
+# baseurl = 'http://minecraft-ids.grahamedgecombe.com/'
+# cssurl = baseurl + 'stylesheets/bundles/all/1440429950.css'
+# imageurl = baseurl + 'images/sprites/items-21.png'
 
-headers = { 'User-Agent' : 'Mozilla/5.0' }
-htmlreq = urllib2.Request(baseurl, None, headers)
-html = urllib2.urlopen(htmlreq).read()
-cssreq = urllib2.Request(cssurl, None, headers)
-css = urllib2.urlopen(cssreq).read()
-imagereq = urllib2.Request(imageurl, None, headers)
-image = urllib2.urlopen(imagereq).read()
+# headers = { 'User-Agent' : 'Mozilla/5.0' }
+# htmlreq = urllib2.Request(baseurl, None, headers)
+# html = urllib2.urlopen(htmlreq).read()
+# cssreq = urllib2.Request(cssurl, None, headers)
+# css = urllib2.urlopen(cssreq).read()
+# imagereq = urllib2.Request(imageurl, None, headers)
+# image = urllib2.urlopen(imagereq).read()
 
-buff = StringIO.StringIO(image)
+# buff = StringIO.StringIO(image)
 
-print 'html length: ' + str(len(html))
+# print 'html length: ' + str(len(html))
 
-im = Image.open(buff)
+# im = Image.open(buff)
 
-print im.size, im.mode
+# print im.size, im.mode
 
-soup = BeautifulSoup(html, "lxml")
-counter = 0
+# soup = BeautifulSoup(html, "lxml")
+# counter = 0
 
-for mat in matstatics:
-	if matstatics[mat]["textures"]:
-		for texname in matstatics[mat]["textures"]:
-			matpath = "./blocks/" + texname + ".png"
-			if os.path.exists(matpath) and texname not in textures:
-				with open(matpath, "rb") as image_file:
-					textures[texname] = base64.b64encode(image_file.read())
+# for mat in matstatics:
+# 	if matstatics[mat]["textures"]:
+# 		for texname in matstatics[mat]["textures"]:
+# 			matpath = "./blocks/" + texname + ".png"
+# 			if os.path.exists(matpath) and texname not in textures:
+# 				with open(matpath, "rb") as image_file:
+# 					textures[texname] = base64.b64encode(image_file.read())
 
-for arow in soup.find_all("tr", class_="row"):
+# for arow in soup.find_all("tr", class_="row"):
 	
-	tofind = arow.div["class"]
-	name = arow.find_all("span", class_="name")[0].getText().encode('ascii','ignore')
-	match = re.search(r"[^a-zA-Z](" + tofind[1] +  ")[^a-zA-Z]", css)
-	num = match.start(1)
+# 	tofind = arow.div["class"]
+# 	name = arow.find_all("span", class_="name")[0].getText().encode('ascii','ignore')
+# 	match = re.search(r"[^a-zA-Z](" + tofind[1] +  ")[^a-zA-Z]", css)
+# 	num = match.start(1)
 	
-	if len( css[num:num+110].split(" ")[2][1:-2]) > 1:
-		dimtoget = ast.literal_eval(css[num:num+110].split(" ")[2][1:-2])
-		cromdim = (0,dimtoget,32,dimtoget+32)
-		allcolors = im.crop(cromdim).getcolors()
-		tofind = arow.find_all("td", class_="id")[0].getText()
+# 	if len( css[num:num+110].split(" ")[2][1:-2]) > 1:
+# 		dimtoget = ast.literal_eval(css[num:num+110].split(" ")[2][1:-2])
+# 		cromdim = (0,dimtoget,32,dimtoget+32)
+# 		allcolors = im.crop(cromdim).getcolors()
+# 		tofind = arow.find_all("td", class_="id")[0].getText()
 
 
-		if ":" in tofind:
-			tofind = tofind.split(":")
-			tofind = int(tofind[0]),int(tofind[1])
-		else:
-			tofind = int(tofind), 0
+# 		if ":" in tofind:
+# 			tofind = tofind.split(":")
+# 			tofind = int(tofind[0]),int(tofind[1])
+# 		else:
+# 			tofind = int(tofind), 0
 
+# 		print  tofind
+# 		newcolors = []
 
-		newcolors = []
-
-		if allcolors:
-			for color in allcolors:
-				if color[1][3] > 0:
-					for x in xrange(1,color[0]):
+# 		if allcolors:
+# 			for color in allcolors:
+# 				if color[1][3] > 0:
+# 					for x in xrange(1,color[0]):
 						
-						newcolors.append((color[1][0], color[1][1], color[1][2], color[1][3]))
-			avgcolor = tuple(map(lambda y: round(sum(y) / float(len(y))/255, 3), zip(*newcolors)))
-			avgnormal = avgcolor[0:3]
-			avgalpha = avgcolor[3]
-			if avgalpha > 0.5:
-				avgalpha = 1
-			material = Material(blockid=tofind,color=avgnormal,name=name,alpha = avgalpha, blandname=name)
-		else:
-			material = Material(blockid=tofind,name=name, blandname=name)
+# 						newcolors.append((color[1][0], color[1][1], color[1][2], color[1][3]))
+# 			avgcolor = tuple(map(lambda y: round(sum(y) / float(len(y))/255, 3), zip(*newcolors)))
+# 			avgnormal = avgcolor[0:3]
+# 			avgalpha = avgcolor[3]
+# 			if avgalpha > 0.5:
+# 				avgalpha = 1
+# 			material = Material(blockid=tofind,color=avgnormal,name=name,alpha = avgalpha, blandname=name)
+# 		else:
+# 			material = Material(blockid=tofind,name=name, blandname=name)
 
 		
-		materials.update({tofind:material.getDict()})
+# 		materials.update({tofind:material.getDict()})
 
-materials.update( material.addRest(materials) )
+# materials.update( material.addRest(materials) )
 
-print "length of materials found on site: " + str(len(materials))
+# print "length of materials found on site: " + str(len(materials))
 
-matsandtex = {"materials":materials,"textures":textures}
+# matsandtex = {"materials":materials,"textures":textures}
 
 
-f.write(repr(matsandtex))
+# f.write(repr(matsandtex))
--- getcolors2.py
+++ getcolors2.py
@@ -19,6 +19,7 @@ blockinfojsonurl = 'http://minecraft-ids.grahamedgecombe.com/items.json'
 imagezipreq = urllib2.Request(imageszipurl, None, headers)
 imagezipfinal = urllib2.urlopen(imagezipreq)
 etag = imagezipfinal.info()['ETag']
+f = open('materials.json', 'w')
 
 fkey = open('./cache/key', 'r')
 fcontent = fkey.read()
@@ -26,9 +27,7 @@ etag = etag.translate(None,'\"')
 
 
 
-if fcontent == etag and os.path.isfile('./cache/items.json') and os.path.isfile('./cache/items.zip'):
-    print 'OK SAME !'
-else:
+if fcontent != etag or not os.path.isfile('./cache/items.json') or not os.path.isfile('./cache/items.zip'):
     fjson = open('./cache/items.json', 'w')
     fzip = open('./cache/items.zip', 'w')
     jsonreq = urllib2.Request(blockinfojsonurl, None, headers)
@@ -40,77 +39,64 @@ else:
     fzip.close()
     fjson.close()
 
-fkey = open('./cache/key', 'w')
-fkey.write(etag)
-fkey.close()
+    fkey = open('./cache/key', 'w')
+    fkey.write(etag)
+    fkey.close()
 
 
-quit()
-    
 
 
 materials = {}
 textures = {}
 
+fzip = open('./cache/items.zip', 'r')
+fjson = open('./cache/items.json', 'r')
 
+itemimagecontents = fzip.read()
+itemcontents = fjson.read()
 
+z = zipfile.ZipFile(StringIO.StringIO(itemimagecontents))
 
+avgcolors = []
 
-
-
-
-
-jsoncontent = jsonreq.read()
-
-
-jsonf = open('./cache/items.json', 'w')
-jsonf.write(jsoncontent)
-
-
-# 
-# imagezip = urllib2.urlopen(imagezipreq).read()
-
-
-
-# z = zipfile.ZipFile(StringIO.StringIO(imagezip))
-
-# avgcolors = []
-
-# with z as archive:
-#     for entry in archive.infolist():
-#         with archive.open(entry) as afile:
+with z as archive:
+    for entry in archive.infolist():
+        with archive.open(entry) as afile:
             
-#             img = Image.open(StringIO.StringIO(afile.read()))
-#             newcolors = []
-#             img = img.convert('RGBA')
-#             allcolors = img.getcolors()
-#             if allcolors:
-#                 for color in allcolors:
-#                     for x in xrange(1,color[0]):
+            img = Image.open(StringIO.StringIO(afile.read()))
+            newcolors = []
+            img = img.convert('RGBA')
+            allcolors = img.getcolors()
+            if allcolors:
+                for color in allcolors:
+                    for x in xrange(1,color[0]):
 
-#                         newcolors.append((color[1][0], color[1][1], color[1][2]))
+                        newcolors.append((color[1][0], color[1][1], color[1][2]))
 
-#                 avgcolors.append(map(lambda y: sum(y) / float(len(y))/255, zip(*newcolors)))
-# print avgcolors
+                avgcolors.append(map(lambda y: sum(y) / float(len(y))/255, zip(*newcolors)))
 
-
-# objects = json.loads(blockinfojson)
-
-# for mat in matstatics:
-#   if matstatics[mat]["textures"]:
-#       for texname in matstatics[mat]["textures"]:
-#           matpath = "./blocks/" + texname + ".png"
-#           if os.path.exists(matpath) and texname not in textures:
-#               with open(matpath, "rb") as image_file:
-#                   textures[texname] = base64.b64encode(image_file.read())
-
-# for aobject in objects:
-#   material = Material(blockid=(aobject['type'], aobject['meta']),name=aobject['name'], blandname=aobject['name'])
-#   materials.update({tofind:material.getDict()})
+objects = json.loads(itemcontents)
+'name'
+for mat in matstatics:
+    if matstatics[mat]["textures"]:
+        for texname in matstatics[mat]["textures"]:
+            matpath = "./blocks/" + texname + ".png"
+            if os.path.exists(matpath) and texname not in textures:
+                with open(matpath, "rb") as image_file:
+                    textures[texname] = base64.b64encode(image_file.read())
 
 
-# materials.update( material.addRest(materials) )
+for aobject in objects:
+    tofind = (aobject['type'], aobject['meta'])
+    material = Material(blockid=(aobject['type'], aobject['meta']),name=aobject['name'].encode('ascii','ignore'), blandname=aobject['name'].encode('ascii','ignore'))
+    materials.update({tofind:material.getDict()})
 
-# print "length of materials found on site: " + str(len(materials))
+# print materials
 
-# matsandtex = {"materials":materials,"textures":textures}
+materials.update( material.addRest(materials) )
+
+print "length of materials found on site: " + str(len(materials))
+
+matsandtex = {"materials":materials,"textures":textures}
+
+f.write(repr(matsandtex))
